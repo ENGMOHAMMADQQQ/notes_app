8c8e2c16286b8cccad1954ac15855703ed7b4e1b
mohammed <hhf5i4yyy@gmail.com>
2023-09-01 15:50:57 +0300

    Add Custom Botton

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:untitled3/constants.dart';
 
+import 'custom_botton.dart';
 import 'custtom_text_field.dart';
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
@@ -8,11 +10,18 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding:  EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        children: [
-        SizedBox(height: 32),
-        CusttomTextField()
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+          SizedBox(height: 32),
+          CusttomTextField(hint: 'Title',),
+          SizedBox(height: 16,),
+          CusttomTextField(hint: 'Contant', maxline: 5,),
+          SizedBox(height: 32,),
+          CustomBotton(),
+          SizedBox(height: 16,),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/custtom_text_field.dart
+++ lib/widgets/custtom_text_field.dart
@@ -3,30 +3,26 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 
 class CusttomTextField extends StatelessWidget {
-  const CusttomTextField({super.key});
-
+  const CusttomTextField({super.key, required this.hint, this.maxline = 1});
+  final String hint;
+  final int maxline;
   @override
   Widget build(BuildContext context) {
-    return  TextField(
+    return TextField(
       cursorColor: kPrimaryColor,
+      maxLines: maxline,
       decoration: InputDecoration(
-        
         border: buildborder(),
-        hintText: 'Title',
+        hintText: hint,
         focusedBorder: buildborder(kPrimaryColor),
         enabledBorder: buildborder(),
       ),
-      
     );
-    
   }
 
   OutlineInputBorder buildborder([color]) {
     return OutlineInputBorder(
-      borderRadius: BorderRadius.circular(8),
-      borderSide: BorderSide(
-        color:color ?? Colors.white
-      )
-      );
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide(color: color ?? Colors.white));
   }
 }
