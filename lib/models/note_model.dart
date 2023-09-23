e776d70e77be5644a526135a3757fbfc610c4e55
mohammed <hhf5i4yyy@gmail.com>
2023-09-23 14:55:19 +0300

    create type adapter for note

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,10 +1,18 @@
-import 'package:flutter/material.dart';
-
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+@HiveType(typeId: 0)
 class NoteModel {
+  @HiveField(0)
   late final String title;
+  @HiveField(1)
   late final String subtitle;
+  @HiveField(2)
   late final String date;
+  @HiveField(3)
   late final int color;
-  NoteModel({required this.title ,required this.subtitle,required this.date ,required this.color});
-  
+  NoteModel(
+      {required this.title,
+      required this.subtitle,
+      required this.date,
+      required this.color});
 }
